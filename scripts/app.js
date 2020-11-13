645b2b1cb6589d37b122d1ab34e0e5c411c6d13e
Clément Baradel <baradelclement@gmail.com>
2020-11-13 23:45:53 +0100

    .

--- scripts/app.js
+++ scripts/app.js
@@ -1,75 +1,118 @@
 getCountries();
 
-async function getCountries() {
+ async function getCountries() {
     const res = await fetch('https://restcountries.eu/rest/v2/all');
     const countries = await res.json();
 
-    displayCountries(countries);
+    displayCountries(countries)
+}
+
+function populationComma(str, n) {
+    var ret = [];
+    var i;
+    var len;
+
+    for(len = str.length, i = len; i > 0; i--) {
+        ret.push(str.substr(i, n))
+    }
+    return ret
 }
 
 function displayCountries(countries) {
     const countriesCards = document.getElementById('countriesCards');
-    countriesCards.innerHTML = ''
+    countriesCards.innerHTML = '';
     countries.forEach(country => {
         const countryEl = document.createElement('div');
         countryEl.classList.add('country-card');
         countryEl.setAttribute('onclick', 'onClickCountryCard(this)');
+        let countryPopulation = (country.population).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+        let curr = country.currencies.forEach(element =>{ const la =  element.name})
         countryEl.innerHTML = `
-        <span class="country-card-img"></span>
+        <span style="background-image: url(${country.flag}); background-size: 100%; background-repeat: no-repeat;" class="country-card-img"></span>
         <div class="country-card-desc">
-            <p class="card-countries-title country-name"></p>
-            <p class="card-desc bold600">Population:<span class="card-desc population"></span></p>
-            <p class="card-desc bold600">Region:<span class="card-desc region"></span></p>
-            <p class="card-desc bold600">Capital:<span class="card-desc capital"></span></p>
+            <p class="card-countries-title country-name">${country.name}</p>
+            <p class="card-desc bold600">Population:<span class="card-desc population"> ${countryPopulation}</span></p>
+            <p class="card-desc bold600">Region:<span class="card-desc region"> ${country.region}</span></p>
+            <p class="card-desc bold600">Capital:<span class="card-desc capital"> ${country.capital}</span></p>
         </div>
         <div class="country-card-desc-detail">
             <a class="back-button bold300" href="/index.html">Back</a>
             <div class="detail-wrap-title">
-                <p class="detail-title country-name"></p>
+                <p class="detail-title country-name"> ${country.name}</p>
             </div>
             <div class="detail-desc-wrap">
                 <div class="detail-desc-wrap-left">
-                    <p class="detail-desc bold600">Native Name: <span id="native-name" class="card-desc"></span></p>
-                    <p class="detail-desc bold600">Population: <span class="card-desc population"></span></p>
-                    <p class="detail-desc bold600">Region: <span class="card-desc region"></span></p>
-                    <p class="detail-desc bold600">Sub Region: <span id="sub-region" class="card-desc"></span></p>
-                    <p class="detail-desc bold600">Capital: <span class="card-desc capital"></span></p>
+                    <p class="detail-desc bold600">Native Name: <span id="native-name" class="card-desc"> ${country.nativeName}</span></p>
+                    <p class="detail-desc bold600">Population: <span class="card-desc population">${countryPopulation}</span></p>
+                    <p class="detail-desc bold600">Region: <span class="card-desc region">${country.region}</span></p>
+                    <p class="detail-desc bold600">Sub Region: <span id="sub-region" class="card-desc">${country.subregion}</span></p>
+                    <p class="detail-desc bold600">Capital: <span class="card-desc capital">${country.capital}</span></p>
                 </div>
                 <div class="detail-desc-wrap-right">
-                    <p class="detail-desc bold600">Top Level Domain: <span class="card-desc" id="domain"></span></p>
-                    <p class="detail-desc bold600">Currencies: <span id="currencies" class="card-desc"></span></p>
-                    <p class="detail-desc bold600">Languages: <span id="languages" class="card-desc"></span></p>
+                    <p class="detail-desc bold600">Top Level Domain: <span class="card-desc" id="domain"> ${country.topLevelDomain}</span></p>
+                    <p class="detail-desc bold600">Currencies: <span id="currencies" class="card-desc">${country.currencies.forEach(element => element.name)} </span></p>
+                    <p class="detail-desc bold600">Languages: <span id="languages" class="card-desc"> ${country.capital}</span></p>
                 </div>
             </div>
             <div class="detail-border-wrap">
-                <p class="detail-desc bold700">Border Countries: <span id="border-countries" class="card-desc"></span></p>
+                <p class="detail-desc bold700">Border Countries: <span id="border-countries" class="card-desc">${country.capital}</span></p>
             </div>
         </div>
         `;
         countriesCards.appendChild(countryEl)
     });
-    // Adjust height of container
-    const countriesCardsLengthNum = 411 * (Math.round(countries.length / 4));
-    const countriesCardsLength = countriesCardsLengthNum.toString();
-    countriesCards.style.height = countriesCardsLength + "px"
 
-    //Blank card
-    const blankCard = document.createElement('div');
-    blankCard.classList.add('blank-card');
-    let i = 0
-    if ((countries.length % 4) == 1) {
-        countriesCards.appendChild(blankCard);
-        console.log(1)
-    } else if ((countries.length % 4) == 2) {
-        while (i != 2) {
-            countriesCards.appendChild(blankCard);
+    // Adjust height of container
+
+    let desktopView = false;
+    let tabletView = false;
+
+    if (window.matchMedia("(min-width: 1300px)").matches) {
+        const countriesCardsLengthNum = 411 * (Math.round(countries.length / 4));
+        const countriesCardsLength = countriesCardsLengthNum.toString();
+        countriesCards.style.height = countriesCardsLength + "px";
+        desktopView = true;
+    } else if ((window.matchMedia("(max-width: 1300px)").matches) &&
+    (window.matchMedia("(min-width: 768px)").matches)) {
+        const countriesCardsLengthNum = 411 * (Math.round(countries.length / 2));
+        const countriesCardsLength = countriesCardsLengthNum.toString();
+        countriesCards.style.height = countriesCardsLength + "px";
+        tabletView = true;
+    } else if (window.matchMedia("(max-width: 768px)").matches) {
+        const countriesCardsLengthNum = 411 * (Math.round(countries.length / 1));
+        const countriesCardsLength = countriesCardsLengthNum.toString();
+        countriesCards.style.height = countriesCardsLength + "px";
+    }
+
+    //Blank card for fill last row
+
+    if (desktopView) {
+        if ((countries.length % 4) == 1) {
+            countriesCards.appendChild(document.createElement('div'));
+        } else if ((countries.length % 4) == 2) {
+            countriesCards.appendChild(document.createElement('div'));
+            countriesCards.appendChild(document.createElement('div'));
+        } else if ((countries.length % 4) == 3) {
+            countriesCards.appendChild(document.createElement('div'));
+            countriesCards.appendChild(document.createElement('div'));
+            countriesCards.appendChild(document.createElement('div'));
         }
-        console.log(2)
-    } else if ((countries.length % 4) == 3) {
-        countriesCards.appendChild(blankCard);
-        countriesCards.appendChild(blankCard);
-        countriesCards.appendChild(blankCard);
-        console.log(3)
+    } else if(tabletView) {
+        if ((countries.length % 4) == 1) {
+            countriesCards.appendChild(document.createElement('div'));
+        }
+    }
+
+    const lastBlankCard = countriesCards.lastElementChild;
+
+    if (lastBlankCard.classList == "") {
+        lastBlankCard.classList.add('blank-card');
+    }
+    if (lastBlankCard.previousElementSibling.classList == "") {
+        lastBlankCard.previousElementSibling.classList.add('blank-card')
+    }
+    if (lastBlankCard.previousElementSibling.previousElementSibling == "") {
+        lastBlankCard.previousElementSibling.previousElementSibling.classList.add('blank-card')
     }
 }
 
@@ -77,10 +120,11 @@ function displayCountries(countries) {
 //Display detail 
 function onClickCountryCard(card) {
     const countryCard = document.querySelectorAll('.country-card')
-    const countriesCards = document.querySelector('.countries-cards')
+    const countriesCards = document.getElementById('countriesCards');
     const searchFilter = document.querySelector('.search-filter')
     const countryDetail = card.lastElementChild;
     
+    countriesCards.style.height = "686px"
     if (!(card.classList.contains('card-detail'))) {
         countryCard.forEach(element => element.classList.toggle('card-detail-out'));
         card.classList.remove('card-detail-out')
